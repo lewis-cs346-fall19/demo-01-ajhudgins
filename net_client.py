398183f7301a4008d6c4e0cde7f9836c5674cbd6
Aaron J Hudgins <ajhudgins@cs.arizona.edu>
2019-09-01 08:10:41 -0700

    Finished simple talking

--- net_client.py
+++ net_client.py
@@ -1,13 +1,17 @@
 import socket 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-addr = ("0.0.0.0", 17897)
+addr = ("0.0.0.0", 17900)
 sock.connect(addr)
-servermsg = sock.recv(1024)
 
+servermsg = sock.recv(1024).decode()
 print(servermsg)
 
-sock.sendall("Hello, this is the client")
+for i in range(0,99):
+	sock.sendall("You're awesome * " + str(i))
+	
+	servermsg = sock.recv(1024).decode()
+	print(servermsg)
 
 sock.close()
 
--- net_server.py
+++ net_server.py
@@ -2,18 +2,21 @@ import socket
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-addr = ("0.0.0.0", 17897)
+addr = ("0.0.0.0", 17900)
 sock.bind(addr)
 sock.listen(5)
 
 while True:
 	(conn, addr) = sock.accept()
 	print("Connected")
-	
-
+			
 	conn.sendall("Hello, this is the server")
-	clientmsg = conn.recv(1024)
-	print(clientmsg)
+	
+	for i in range(0,99): 
+		clientmsg = conn.recv(1024).decode()
+		print(clientmsg)
 
+		conn.sendall(clientmsg[0:14] + ", back at ya * " + str(i * i))
+  
 	conn.close()
 
